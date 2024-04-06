8037c0a8bac4a6f988464b1603dc87cd5ac3bfc9
pittner <lubos.pittner@posam.sk>
2024-04-06 11:26:03 +0200

    calc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,24 @@
 import {Component} from '@angular/core';
 import {CounterComponent} from './counter/counter.component';
 import {NgIf} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     CounterComponent,
-    NgIf
+    NgIf,
+    FormsModule
   ],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'course-quest';
   counterVisible = true;
+
+  valueA = 0;
+  valueB = 0;
 
   toggleCounters() {
     this.counterVisible = !this.counterVisible;
