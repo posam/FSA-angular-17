cf54a07c832857a7a87a760b573f252d5d5d30b3
pittner <lubos.pittner@posam.sk>
2024-04-06 12:08:13 +0200

    bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,16 @@
 import {Component} from '@angular/core';
-import {CounterComponent} from './counter/counter.component';
-import {NgIf} from '@angular/common';
-import {FormsModule} from '@angular/forms';
-import {CalcComponent} from './calc/calc.component';
-import {CountersComponent} from './counters/counters.component';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    CounterComponent,
-    NgIf,
-    FormsModule,
-    CalcComponent,
-    CountersComponent
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'course-quest';
-
 
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {CalcComponent} from './calc/calc.component';
+import {CountersComponent} from './counters/counters.component';
+import {HomeComponent} from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  { path: 'home', component: HomeComponent },
+  { path: 'calc', component: CalcComponent },
+  { path: 'counter', component: CountersComponent },
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '**', component: PageNotFoundComponent}
+
+];
