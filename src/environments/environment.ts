11cf935102c253cc7c0891d98390c7c83b5a5529
Radovan Pieter <radovan.pieter@posam.sk>
2024-04-15 23:32:39 +0200

    Fix Environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  keyCloakUrl: 'fsa-keycloak-http.app.svc.cluster.local/auth/realms/fsa',
-  beUrl: 'fsa-be.app.svc.cluster.local',
-  appUrl: 'http://localhost:4200',
+  keyCloakUrl: 'fsa-keycloak-http.app.svc.cluster.local',
+  beUrl: 'fsa-be.app.svc.cluster.local:8080',
+  appUrl: 'fsa-fe.app.svc.cluster.local',
 };
