92b8043b4cda762e4c6b17443ef7ce5671a1f0e5
Radovan Pieter <radovan.pieter@posam.sk>
2024-04-16 17:54:30 +0200

    Fix Environments v8

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  keyCloakUrl: 'http://52.138.207.76:8081',
-  beUrl: 'http://52.138.207.76:8080',
-  appUrl: 'http://52.138.207.76:80',
+  keyCloakUrl: 'http://52.138.207.76',
+  beUrl: 'http://52.138.207.76',
+  appUrl: 'http://52.138.207.76',
 };
