d31f986c2ba08f5c56a0daeb1e77a91bc27f24b8
Radovan Pieter <radovan.pieter@posam.sk>
2025-04-15 19:51:45 +0200

    Fix https in appUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   keyCloakUrl: 'https://app.fullstackacademy.sk/auth',
   beUrl: 'https://app.fullstackacademy.sk',
-  appUrl: 'http://app.fullstackacademy.sk',
+  appUrl: 'https://app.fullstackacademy.sk',
 };
