4208793e6b427241464a746d6200420125dc0fc4
Radovan Pieter <radovan.pieter@posam.sk>
2025-04-15 19:12:26 +0200

    Zmena Keycloak URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  keyCloakUrl: 'https://keycloak.fullstackacademy.sk',
+  keyCloakUrl: 'https://keycloak.fullstackacademy.sk/auth',
   beUrl: 'https://app.fullstackacademy.sk',
   appUrl: 'http://app.fullstackacademy.sk',
 };
